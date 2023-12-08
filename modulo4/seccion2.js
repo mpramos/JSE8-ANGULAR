35fbf7237b440366e0f5edfcf0de2016c2211ee5
maria <mpramos78915400@gmail.com>
2023-12-07 21:47:24 -0400

    laboratorio seccion2

---
+++ modulo4/seccion2.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 10; i++) { /// 11 <=10; i=11
+    console.log('juana')    
+}
+
+let j=1
+while (j<=10) { //11<=10
+    console.log('juan')    
+    j++ //11
+}
+
+let num=6
+do {
+    console.log(num);
+    num++
+} while (num<=5);
+
+
+//
+
+let values = [10, 30, 50, 100];
+let sum = 0;
+for (let i = 0; i < 4; i++) { // 1<4
+    sum += values[i]; // sum = 10
+    //sum= sum + values[i]
+}
+console.log(sum); // -> 190
+
